504e6fc780eab04396be368e40315bfd02f47186
GiugioiuMarian <38139536+MarianGiugioiu@users.noreply.github.com>
2020-10-21 14:42:21 -0700

    Player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    private Vector2 movement;
+    private Vector2 mousePos;
+
+    public Rigidbody2D rbPlayer;
+    public Rigidbody2D rbWeapon;
+    public Camera cam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        float sign = Mathf.Sign(rbPlayer.position.x - mousePos.x);
+
+        float valuePX = Mathf.Abs(rbPlayer.transform.localScale.x);
+        float valuePY = rbPlayer.transform.localScale.y;
+        float valuePZ = rbPlayer.transform.localScale.z;
+
+        float valueWX = Mathf.Abs(rbWeapon.transform.localScale.x);
+        float valueWY = Mathf.Abs(rbWeapon.transform.localScale.y);
+        float valueWZ = rbWeapon.transform.localScale.z;
+        if (sign > 0)
+        {
+            rbPlayer.transform.localScale = new Vector3(-valuePX, valuePY, valuePZ);
+            rbWeapon.transform.localScale = new Vector3(-valueWX, valueWY, valueWZ);
+        }
+        else
+        {
+            rbPlayer.transform.localScale = new Vector3(valuePX, valuePY, valuePZ);
+            rbWeapon.transform.localScale = new Vector3(valueWX, valueWY, valueWZ);
+        }
+
+        rbPlayer.MovePosition(rbPlayer.position + movement * movementSpeed * Time.fixedDeltaTime);
+        rbWeapon.MovePosition(rbPlayer.position);
+
+        Vector2 lookDir = mousePos - rbPlayer.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rbWeapon.rotation = angle;
+    }
+}
---
+++ Assets/Scripts/TakesDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TakesDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Image healthBar;
+    private float maxHealth;
+    private float height;
+    private void Start()
+    {
+        maxHealth = healthBar.rectTransform.sizeDelta.x;
+        height = healthBar.rectTransform.sizeDelta.y;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            //Debug.Log(health.rectTransform.sizeDelta);
+            float width = healthBar.rectTransform.sizeDelta.x;
+            
+            if(width > 0)
+            {
+                Debug.Log("Hit");
+                healthBar.rectTransform.sizeDelta = new Vector2(width - 0.1f * maxHealth, height);
+            } else
+            {
+                Debug.Log("Dead");
+            }
+        }
+    }
+}
