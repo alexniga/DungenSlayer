6c587384942d17236c39e421ddbceb650aca96f0
alexniga <46327732+alexniga@users.noreply.github.com>
2021-01-16 10:12:02 +0200

    skip cutscene

--- Assets/MangeCutScenes.cs
+++ Assets/MangeCutScenes.cs
@@ -8,17 +8,42 @@ public class MangeCutScenes : MonoBehaviour
     private string nextLevelName;
     private string currentLevelName;
     public int delay;
+    float timer;
     private void Start()
     {
         currentLevelName = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>().CurrentLevel();
         nextLevelName = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>().IncreaseLevel();
-        StartCoroutine("ChangeTheScene");
+        //StartCoroutine("ChangeTheScene");
+        timer = delay;
     }
 
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(nextLevelName, LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync(currentLevelName);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            timer = 0f;
+        }
+
+    }
     IEnumerator ChangeTheScene()
     {
         print(delay);
-        yield return new WaitForSeconds(delay);
+        for (int i = 1; i <= delay; i++)
+        {
+            yield return new WaitForSeconds(1f);
+            if (Input.GetMouseButtonDown(0))
+            {
+                break;
+            }
+        }
+        
         print(nextLevelName);
         print(currentLevelName);
         SceneManager.LoadScene(nextLevelName, LoadSceneMode.Additive);
